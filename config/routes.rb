9f3f89d91730f550f10b9e78116fe7ce01c88172
sonali ramnarain <sonaliramnarain01@gmail.com>
2021-11-09 15:03:58 +0400

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   resources :boyfriends, :index :search_by_categroy :show do
-   resources :bookings, :create :index do
-
-   resources :reviews, :new
+  resources :boyfriends do
+   resources :bookings
   end
 end
