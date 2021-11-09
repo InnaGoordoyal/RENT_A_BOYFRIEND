1bb9cdb146b4be81a1c9340c79cf8c053e60d49f
inna goordoyal <innagoordoyal@gmail.com>
2021-11-09 14:08:38 +0400

    review model

--- app/models/boyfriend.rb
+++ app/models/boyfriend.rb
@@ -1,5 +1,5 @@
 class Boyfriend < ApplicationRecord
  has_many :users, :bookings, :reviews
 
- validates :first_name, :last_name, :age, :category, :email presence: true
+ validates :first_name, :last_name, :age, :category, :email, presence: true
 end
