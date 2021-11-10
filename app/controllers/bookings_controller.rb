9cc91ff35b6bc741aa7c61909f53eb3bb4267458
hansvinvenetheethan <hansvin18@gmail.com>
2021-11-10 12:23:00 +0400

    booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,7 +7,12 @@ class BookingsController < ApplicationController
     @bookings = bookings.all
   end
 
- #def create
-  #@bookings = @Bookings.new(booking_params)
- #end
+ def create
+  @bookings = @Booking.new(booking_params)
+ end
+
+ def new
+  @boyfriend = Boyfriend.find(params[:boyfriend_id])
+  @booking = Booking.new
+ end
 end
