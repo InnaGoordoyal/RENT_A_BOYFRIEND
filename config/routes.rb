121ce59a266d8bb60ca55d34091428d5c4e78df9
sonali ramnarain <sonaliramnarain01@gmail.com>
2021-11-12 12:36:48 +0400

    profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :boyfriends do
-   resources :bookings
+   resources :boyfriends do
+    resources :bookings
   end
 end
